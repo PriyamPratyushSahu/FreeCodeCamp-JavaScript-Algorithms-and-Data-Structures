f72b7ab85e6799d7861f7ab528206d4381628c73
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-22 11:50:45 +0530

    14_Division

--- 1_Basic JavaScript/13_Multiplication.js
+++ 1_Basic JavaScript/13_Multiplication.js
@@ -10,6 +10,6 @@ myVar would have the value 169.
 */
 
 //Question: Change the 0 so that product will equal 80.
-const product = 8 * 0;
+//const product = 8 * 0;
 /*************************** Solution ****************************/
 const product = 8 * 10;
