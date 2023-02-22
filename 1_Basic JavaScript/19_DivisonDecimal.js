1014a3ce75b19a67fd84ebaa0de9373019bfd783
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-22 12:31:57 +0530

    19_DivisionDecimal

---
+++ 1_Basic JavaScript/19_DivisonDecimal.js
@@ -0,0 +1,8 @@
+/*
+Now let's divide one decimal by another.
+*/
+
+//Question: Change the 0.0 so that quotient will equal to 2.2.
+//const quotient = 0.0 / 2.0; // Change this line
+/*************************** Solution ****************************/
+const quotient = 4.4 / 2.0; // Change this line
