15ec90cd1ac0716f64e97555a74de0243b39cc3a
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-28 16:45:38 +0530

    40_Arrays2

---
+++ 1_Basic JavaScript/40_Arrays2.js
@@ -0,0 +1,22 @@
+/*
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array.
+
+
+*/
+
+//Question: Create a nested array called myArray.
+/*
+// Only change code below this line
+const myArray = [];
+*/
+
+/*************************** Solution ****************************/
+// Only change code below this line
+const myArray = [
+    [1,2,3,4],
+    ['a','b','c','d'],
+    ['abc','def','ghi', 'jkl']
+  ];
